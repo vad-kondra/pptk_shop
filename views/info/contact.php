c5e8c841f7cfb2d2bf2b64eb22f89aa1c201d2cd
vad-kondra <vad-kondra@yandex.ua>
2020-06-22 16:38:51 +0300

    refactoring

--- views/info/contact.php
+++ views/info/contact.php
@@ -12,12 +12,6 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
 
 ?>
 
-<!--<div class="container">-->
-<!--    <div class="map">-->
-<!--        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d168931.07033039996!2d39.21654015413025!3d48.580205822878554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sua!4v1555207622513!5m2!1sen!2sua" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>-->
-<!--    </div>-->
-<!--</div>-->
-
 <div class="align-center pb-5">
     <div class="container">
         <div class="contact-info">
@@ -32,6 +26,9 @@ $this->params['breadcrumbs'][] = ['label' => $this->title];
                         'URI.SafeIframeRegexp'=>'%^(https?:)?//(www\.youtube(?:-nocookie)?\.com/embed/|player\.vimeo\.com/video/)%',
                     ]) ?>
                 </div>
+            </div>
+            <div class="row pt-40">
+                <div style="width: 100%"><iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=ru&amp;coord=48.548014, 39.331321&amp;q=%D0%B3.%20%D0%9B%D1%83%D0%B3%D0%B0%D0%BD%D1%81%D0%BA%2C%20%D1%83%D0%BB.%20%D0%9E%D0%B1%D0%BE%D1%80%D0%BE%D0%BD%D0%BD%D0%B0%D1%8F%2C%2024+(%D0%9E%D0%9E%D0%9E%20%C2%AB%D0%9F%D0%9F%D0%A2%D0%9A%C2%BB)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="http://www.gps.ie/">GPS coordinates</a></iframe></div><br />
             </div>
 <!--            <div class="row m-0">-->
 <!--                <div class="col-sm-4 p-0">-->
